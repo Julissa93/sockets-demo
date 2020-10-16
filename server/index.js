3416a8e2b6622e3080ebd10ced24fb5ad5f38622
Julissa Napoletano <jnapoletano28@gmail.com>
2020-10-16 11:56:16 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,22 +14,20 @@ app.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
 
-//DEMO
-//1. npm i socket.io
-//2. set up on "connection" function and console.log when someone connects to server
-//3. set up event handler for when new message comes in from client
-//4. but client socket is not set up - like we said earlier in lecture, socket io is a DUET of libraries made for the client and server side
-// to complete the connection set up we must do this for the client AND the server
-//5. go to client/socket.js and add io code 
-//6. now in order to emit events we use socket.emit 
-//7. in order to handle incoming events we use socket.on
-//8. and since we are using sockets, both client and server can emit or listen to events
-// 9. Now that we have our socket connection between client and server, now we can start emitting and handling events
+/* 
+- To emit events we use socket.emit()
+- To handle incoming events we use socket.on()
+- To send message to everyone except the sender we use socket.broadcast.emit()
+- To send message to everying INCLUDING sender we use io.emit() (NOTE: in class I made a mistake and said it was socket.emit - I meant io.emit()!!!)
+*/
 
-
+//set up on "connection" event handler and console.log the socket.id when someone connects to server
 io.on('connection', socket => {
     console.log(socket.id, ' has made a persistent connection to the server!')
-
+    
+    //set up event handler (socket.on) for when new message comes in from client - BUT our client socket is not set up yet. 
+    //like we said earlier in lecture, socket io is a DUET of libraries made for the client and server side. 
+    //So to complete the websocket connection w/ socket.io we must do create a socket object for both client and server! (client socket is in client/socket.js)
     socket.on('new-text-from-client', text => { 
         //console.log('text: ', text) to see what you get from client
         //What should we use to send message to everyone EXCEPT the sender? socket.broadcast
